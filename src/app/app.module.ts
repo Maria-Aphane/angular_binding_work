86662b3faea79d5f89728ed2cdf89e71562543cf
Aphane Maria <mariaaphane17@gmail.com>
2018-06-22 11:45:41 +0200

    binding work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/name/name.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit ,Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-name',
+  templateUrl: './name.component.html',
+  styleUrls: ['./name.component.css']
+})
+export class NameComponent implements OnInit {
+
+
+  name:string;
+  sname : string;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+ 
+
+}
